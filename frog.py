89d433007021bc16690a9f726c47c035b88ecfc2
yurawu27 <140979715+yurawu27@users.noreply.github.com>
2024-03-11 18:27:01 -0400

    fix minor issues

--- frog.py
+++ frog.py
@@ -20,7 +20,7 @@ def help():
 
 
 def hello(user):
-    print('\U0001F438: Hi there' + user + '!')
+    print('\U0001F438: Hi there ' + user + '!')
 
 
 def encourage():
@@ -33,23 +33,23 @@ def frogmode():
 
 def feed(snack):
     cap_snack = snack.capitalize()
-    print("\U0001F438: Thanks for the sweet little treat! " + cap_snack + "is yummy! nom nom nom")
+    print("\U0001F438: Thanks for the sweet little treat! " + cap_snack + " is yummy! nom nom nom")
     
     
 def kill(anything):
-    frogs_decision = random(1, 2)
+    frogs_decision = random.randint(1, 2)
     
     # frog is willing to kill for you!
     if frogs_decision == 1:
-        print("\U0001F438: I killed" + anything + "for you! You are safe now!")
+        print("\U0001F438: I killed " + anything + " for you! You are safe now!")
     
     # nuarr the frog is a pacifist
     else:
-        print("\U0001F438: I don't like" + anything + ", I only eat yummy snacks! nom nom nom")
+        print("\U0001F438: I don't like " + anything + ", I only eat yummy snacks! nom nom nom")
     
     
 def goodbye(user):
-    print('\U0001F438: Bye bye' + user+ ', go catch some flies!')
+    print('\U0001F438: Bye bye ' + user+ ', go catch some flies!')
     
 
 
