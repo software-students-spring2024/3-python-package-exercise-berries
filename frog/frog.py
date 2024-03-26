8e2383abbc0f929a30fc861193a70e3b630e8e30
BonnyCChavarria <bcb392@nyu.edu>
2024-03-26 14:23:21 -0400

    Set up new version for testpypi

--- frog/frog.py
+++ frog/frog.py
@@ -2,19 +2,6 @@ import random
 
 # function that lists all the available functions in the module
 ### the parser help is used instead ###
-### help() not used in main, can be deleted/commented out ###
-'''
-def help():
-    
-    print("hello(your name): the frog says hello to you ;)  (replace 'your name' with your name!)")
-    print("encourage(): the frog gives you a silly little pep talk <3")
-    print("frogmode(): the frogs gets into frog mode and eats bugs with you! ")
-    print("feed(snack): feed the frog a yummy snack!    (replace 'snack' with your choice of snack!)")
-    print("kill(anything): the fighter frog kills anything for you!  (replace 'anything' with anything you want to kill!)")
-    print("goodbye(your name): frog says goodbye to you!  (replace 'your name' with your name!)")
-'''
-
-    
     
 # ---------------------------------------------------------------------------- #
 #                              available functions                             #
